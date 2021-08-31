882ca1691e751ed34e750fdc8ce3340dc6c4df16
claudia_alarcon <c.alarconlazo@gmail.com>
2021-08-31 12:28:05 -0400

    se agrega modal login, para obtener token y almacenarlo en localstorage

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -2,6 +2,50 @@
 
 const baseUrl = "http://localhost:3000/api"
 
+
+$('#form-login').submit(async(event) => {
+  event.preventDefault();
+  const email = document.getElementById('input-email').value;
+  const password = document.getElementById('input-password').value;
+  console.log(email, password);
+  const JWT = await postData(email, password);
+  console.log(JWT);
+  hideShowNavBar(JWT);
+});
+
+//Enviar credenciales y obtener token
+const postData = async(email, password) => {
+  try {
+    const response = await fetch('http://localhost:3000/api/login', 
+    {
+      method: 'POST',
+      body: JSON.stringify({email:email, password:password})
+    })
+    const {token} = await response.json();
+    localStorage.setItem('jwt-token', token);
+    return token;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+//Ocultar botones de navBar
+function hideShowNavBar(jwt) {
+  try {
+    if (jwt !== '') {
+      console.log('pasamos');
+      $('#login').toggle();
+      $('#situacion-chile').toggle();
+      $('#logout').toggle();
+      $('#login-modal').modal('hide');
+    } else {
+      console.log('no pasamos');
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
 async function detalleInformacion(url){
   const response = await fetch (url);
   const { data } = await response.json();
